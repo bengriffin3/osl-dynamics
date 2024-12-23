e6e4785d961609e18c69033a3b503a1b465c75f1
weiym97 <weiym97@163.com>
2024-12-23 16:20:42 +0000

    WIP, plot ncv

--- osl_dynamics/config_api/batch.py
+++ osl_dynamics/config_api/batch.py
@@ -707,9 +707,12 @@ class BatchAnalysis:
             save_dir = config['save_dir']
             mode = config['mode']
             if 'ncv' in mode and int(n_states)>1:
+                print(type(model))
+                print(type(n_states))
+                print('#################33')
                 #try:
-                    with open(f'{save_dir}/ncv_free_energy.json','r') as file:
-                        free_energy[model][str(int(n_states))].append(float(json.load(file)[0]))
+                with open(f'{save_dir}/ncv_free_energy.json','r') as file:
+                    free_energy[model][str(int(n_states))].append(float(json.load(file)[0]))
                 #except Exception:
                 #    print(f'save_dir {save_dir} fails!')
 
