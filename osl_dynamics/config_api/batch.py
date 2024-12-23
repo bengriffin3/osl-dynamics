060cfdff4b0d3389cd4597570375d56433484ae7
weiym97 <weiym97@163.com>
2024-12-23 02:38:05 +0000

    WIP, plot naive cv

--- osl_dynamics/config_api/batch.py
+++ osl_dynamics/config_api/batch.py
@@ -709,7 +709,7 @@ class BatchAnalysis:
             if 'ncv' in mode and int(n_states)>1:
                 try:
                     with open(f'{save_dir}/ncv_free_energy.json','r') as file:
-                        free_energy[model][str(int(n_states))] = float(json.load(file)[0])
+                        free_energy[model][str(int(n_states))].append(float(json.load(file)[0]))
                 except Exception:
                     print(f'save_dir {save_dir} fails!')
 
