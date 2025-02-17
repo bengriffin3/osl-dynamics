eb01812f90e4a4b0570bf26025dccde9164862d0
weiym97 <weiym97@163.com>
2025-02-17 11:41:31 +0000

    WIP, ll before silencing

--- osl_dynamics/config_api/batch.py
+++ osl_dynamics/config_api/batch.py
@@ -1165,7 +1165,7 @@ class BatchAnalysis:
 
         ll_before_silencing = []
         for n_state in n_states:
-            with open(f'{self.config_root}/{model}_state_{n_state}/mode/Y_test/metrics.json', 'r') as file:
+            with open(f'{self.config_path}/{model}_state_{n_state}/mode/Y_test/metrics.json', 'r') as file:
                 metric = json.load(file)[object]
             ll_before_silencing.append(metric)
 
