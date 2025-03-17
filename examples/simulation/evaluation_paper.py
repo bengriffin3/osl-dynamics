2a84a1dce7e0e7678a09241d73b9d84f0744abc7
weiym97 <weiym97@163.com>
2025-03-17 18:22:47 +0000

    Add soft mixing to simulation

--- examples/simulation/evaluation_paper.py
+++ examples/simulation/evaluation_paper.py
@@ -167,6 +167,38 @@ def swc_hrf(save_dir,n_subjects,n_samples,n_states,n_channels,tr):
         np.savetxt(f'{save_dir}{10001 + i}.txt', apply_hrf(data[i],tr))
         np.save(f'{save_dir}truth/{10001 + i}_state_time_course.npy', time_course[i])
 
+def soft_mixing(save_dir):
+    save_dir = f'{save_dir}/soft_mixing/'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    if not os.path.exists(f'{save_dir}truth/'):
+        os.makedirs(f'{save_dir}truth/')
+
+    n_subjects = 64
+    n_channels = 80
+    n_modes = 6
+    sim = simulation.MixedSine_MVN(
+        n_samples=25600,
+        n_modes=n_modes,
+        n_channels=n_channels,
+        relative_activation=[1, 0.5, 0.5, 0.25, 0.25, 0.1],
+        amplitudes=[6, 5, 4, 3, 2, 1],
+        frequencies=[1, 2, 3, 4, 6, 8],
+        sampling_frequency=250,
+        means="zero",
+        covariances="random",
+    )
+
+    time_course = sim.mode_time_course
+    data = sim.time_series
+    data = data.reshape(n_subjects, -1, n_channels)
+    time_course = time_course.reshape(n_subjects, -1, n_modes)
+
+    np.save(f'{save_dir}truth/state_covariances.npy', sim.obs_mod.covariances)
+
+    for i in range(n_subjects):
+        np.savetxt(f'{save_dir}{10001 + i}.txt', data[i])
+        np.save(f'{save_dir}truth/{10001 + i}_mode_time_course.npy', time_course[i])
 def main(simulation_list=None):
     save_dir = './data/node_timeseries/simulation_final/'
     if not os.path.exists(save_dir):
@@ -192,3 +224,6 @@ def main(simulation_list=None):
         swc_iid(**config)
     if 'swc_hrf' in simulation_list:
         swc_hrf(**config)
+
+    if 'soft_mixing' in simulation_list:
+        soft_mixing(save_dir)
