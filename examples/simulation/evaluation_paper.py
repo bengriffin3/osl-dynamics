f98c2739aa2a69bdd41a5aa5292657bc34c65d75
weiym97 <weiym97@163.com>
2025-03-26 11:03:54 +0000

    DyNeMo final simulation

--- examples/simulation/evaluation_paper.py
+++ examples/simulation/evaluation_paper.py
@@ -138,7 +138,6 @@ def dynemo_iid_final(save_dir, n_subjects, n_samples, n_states, n_channels, tr):
         sampling_frequency=250,
         means="zero",
         covariances="random",
-        random_seed=123,
     )
 
     data = sim.time_series
